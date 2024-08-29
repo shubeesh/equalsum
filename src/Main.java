a6cde777b840e29b6a673c3a2125aedff6772c79
shubeeth001 <shubeeth001@warren.k12.in.us>
2024-08-29 12:52:01 -0400

    equalsum

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));
+        System.out.println(hasEqualSum(1, 1, 2));
+        System.out.println(hasEqualSum(1, -1, 0));
+    }
+
+    private static int hasEqualSum(int num1, int num2, int num3) {
+        num1 = 15;
+        num2 = 23;
+        int sum = 38;
+
+        return sum;
+    }
+
+}
